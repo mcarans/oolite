663ddac7933c31ec3117b2e7d419dc0bcf40a0fd
mcarans <rans@email.com>
2026-07-09 19:04:25 +1200

    Wrong path

--- installers/win32/create_nsis_fn.sh
+++ installers/win32/create_nsis_fn.sh
@@ -14,7 +14,7 @@ create_nsis() {
     cd ../../build/nsis
     local oolite_dir="../$build_folder"
     local ver_full ver_quad githash buildtime app_date
-    parse_manifest ver_full ver_quad githash buildtime app_date "../$oolite_dir/Resources/manifest.plist"
+    parse_manifest ver_full ver_quad githash buildtime app_date "$oolite_dir/Resources/manifest.plist"
 
     cp ../../installers/win32/* ./
 
